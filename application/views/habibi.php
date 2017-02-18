423be25b85e311ea12b9a8f1f7caa91e06878732
irsanrajamin <irsanrajamin@gmail.com>
2017-02-18 13:38:13 +0700

    versi 2.3.2

---
+++ application/views/habibi.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Habibi Garden</title>
+	<meta charset="utf-8">
+	<meta name="author" content="pixelhint.com">
+	<meta name="description" content="Notify Under Constraction and Coming Soon Template"/>
+
+	<link rel="shortcut icon" href="<?php echo base_url('assets/img/habibi_logo.ico')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/reset.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/main.css')?>">
+
+    <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/TimeCircles.js') ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/backstretch.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/main.js') ?>"></script>
+</head>
+<body>
+
+	<section class="content wrapper">
+	<img src="<?php echo base_url('assets/img/logo.png') ?>">
+		<h1>Habibi Garden</h1>
+		<h3 style="margin-top: 10px;">Website Is Under Construction</h3>
+		
+
+		<div class="subscription_form clearfix">
+		</div>
+
+		<div class="counter clear" data-date="2017-02-25 00:00:00"></div>
+		<p style="margin-top: 30px;"><h3>Call : 082129566512 / Email : irsanrajamin@gmail.com</h3></p>
+	</section>
+	
+	<div class="back_img"></div>
+	
+</body>
+</html>
